5bcd7b6ff5b41d4b2215acaa4fc7110af0354f7c
Anelya <kurbanovaanela4@gmail.com>
2024-03-28 14:37:14 +0500

    add lab5

---
+++ lab5/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent }
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    CategoriesComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  
+})
+
+export class AppModule { }
